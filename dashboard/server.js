1381cc240eb1f87a26a44b184a7e9238f2352f9d
theADAMJR <adamjf123@gmail.com>
2020-06-24 10:43:27 +0100

    add bootstrap cdn and basic styles

--- dashboard/server.js
+++ dashboard/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/')
+console.log(__dirname);
 
-app.listen(3000, () => console.log('Server is live on port 3000!'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => res.render(`home`));
+app.get('/invite', (req, res) => res.redirect('some link'));
+
+app.listen(3000,
+  () => console.log('Server is live on port 3000!'));
+
+  // 7EC1BB, 71B1B1, BC9957, E0A755, EFC08D
