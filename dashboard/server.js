18527ff83ca7318d361631bc5f454c0e34ab6297
theADAMJR <adamjf123@gmail.com>
2020-08-29 14:39:52 +0100

    add an index route

--- dashboard/server.js
+++ dashboard/server.js
@@ -2,5 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.send('<h1>Hi!</h1>'));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is live on port ${port}`));
