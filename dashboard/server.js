de392c43460e788c77fb2397c8bb5bce3a5174c6
theADAMJR <adamjf123@gmail.com>
2020-09-01 11:08:11 +0100

    import bootstrap + add navbar and jumbotron

--- dashboard/server.js
+++ dashboard/server.js
@@ -5,9 +5,7 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => res.render('index', {
-  something: 'testing 321 123'
-}));
+app.get('/', (req, res) => res.render('index'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is live on port ${port}`));
