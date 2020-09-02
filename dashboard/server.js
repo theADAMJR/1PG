8c5008bccdcff0ba6113e3cd409876ec368aa9bc
theADAMJR <adamjf123@gmail.com>
2020-09-02 11:20:52 +0100

    add custom css + add icons

--- dashboard/server.js
+++ dashboard/server.js
@@ -5,6 +5,9 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+app.use(express.static(`${__dirname}/assets`));
+app.locals.basedir = `${__dirname}/assets`;
+
 app.get('/', (req, res) => res.render('index'));
 
 const port = process.env.PORT || 3000;
