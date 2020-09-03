4bfcffe71106733ab9936f09df93737d0cdfaf81
theADAMJR <adamjf123@gmail.com>
2020-09-03 11:24:44 +0100

    add commands page

--- dashboard/server.js
+++ dashboard/server.js
@@ -9,6 +9,14 @@ app.use(express.static(`${__dirname}/assets`));
 app.locals.basedir = `${__dirname}/assets`;
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/commands', (req, res) => res.render('commands', {
+  subtitle: 'Commands',
+  categories: [
+    { name: 'Auto Mod', icon: 'fas fa-gavel' },
+    { name: 'Economy', icon: 'fas fa-coins' }, 
+    { name: 'General', icon: 'fas fa-star' },
+    { name: 'Music', icon: 'fas fa-music' }]
+}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is live on port ${port}`));
