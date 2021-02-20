1fecb7e407f3f6edf2b910d90c8fc8f62dc0373e
Pixelwarp <67162012+Pixelwarp@users.noreply.github.com>
2021-02-20 17:44:58 +0100

    Update guild-member-add.js

--- handlers/events/guild-member-add.js
+++ handlers/events/guild-member-add.js
@@ -2,7 +2,7 @@ const Event = require('./event');
 const logs = require('../../data/logs');
 
 module.exports = class extends Event {
-  on = 'guildMemberRemove';
+  on = 'guildMemberAdd';
   
   async invoke(msg) {
     await logs.add(msg.guild.id, 'joins');
