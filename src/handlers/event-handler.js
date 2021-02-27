c2d97aa5382e0b207b57145ba7d5d19850092bf8
theADAMJR <adamjf123@gmail.com>
2021-02-27 16:20:28 +0000

    Update event-handler.js

--- src/handlers/event-handler.js
+++ src/handlers/event-handler.js
@@ -1,14 +1,20 @@
 import { readdirSync } from 'fs';
+import Deps from '../utils/deps.js';
+import { Client } from 'discord.js';
 
 export class EventHandler {
-  async init(bot) {
+  constructor(bot = Deps.get(Client)) {
+    this.bot = bot;
+  }
+
+  async init() {
     const fileNames = readdirSync(`./src/handlers/events`);
     for (const fileName of fileNames) {
       const { default: Event } = await import(`./events/${fileName}`);
       const event = new Event();
       if (!event.on) continue;
 
-      bot.on(event.on, event.invoke.bind(event));
+      this.bot.on(event.on, event.invoke.bind(event));
     }
     console.log(`Listened for ${fileNames.length - 1} events`);
   }
