93d3977587eefaa2fa48e514b898693f5ea1569b
theADAMJR <adamjf123@gmail.com>
2021-02-27 16:21:02 +0000

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -12,7 +12,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
 });
 
-export const bot = Deps.get(Client);
+// you may want client options here 
+export const bot = Deps.add(Client, new Client());
 bot.login(process.env.BOT_TOKEN);
 
 Deps.get(EventHandler).init();
